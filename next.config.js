17cc8cfb990c064e8a8344bad838712b6cc7278a
Prafulla Pal <prafullpal18@gmail.com>
2023-04-20 02:36:36 +0530

    Tailwind Setup, Navbar added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "d33wubrfki0l68.cloudfront.net",
+      },
+      {
+        protocol: "https",
+        hostname: "flowbite.com",
+      },
+    ],
+    dangerouslyAllowSVG: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body>
+    <Html lang="en" className="dark">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body className="text-slate-900 dark:text-white dark:bg-gray-700">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
