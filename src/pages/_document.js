e964da454a1626e506191e8565ead63af5925975
Prafulla Pal <44419039+prafullapal@users.noreply.github.com>
2023-07-10 12:30:24 +0000

    Redirect to vercel

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta http-equiv="refresh" content="0; URL=https://portfolio-prafullapal.vercel.app/"/>
+        <link rel="canonical" href="https://portfolio-prafullapal.vercel.app/"/>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body className="text-whiteText">
